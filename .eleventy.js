94494290ff99e9b10c7572dd1e1bf10ba17e6903
Arriana Blais <arriBlais@gmail.com>
2021-05-15 17:53:26 -0400

    fix build problems add faviocn

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+module.exports = {
+	dir: {
+		input: 'src',
+		output: '_site'
+	},
+	templateFormats: [
+		"md",
+		"html",
+		"css",
+		"jpg",
+		"png",
+		"gif",
+		"ico",
+		"svg",
+		"njk",
+		"ttf",
+		"otf",
+		"woff",
+		"woff2",
+	],
+	passthroughFileCopy: true,
+}
