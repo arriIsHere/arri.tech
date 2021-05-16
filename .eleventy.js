680c5ecb6ff659905f353287da3cadd526f95839
Arriana Blais <arriBlais@gmail.com>
2021-05-16 12:26:01 -0400

    and ids to headers

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,41 @@
-module.exports = {
-	dir: {
-		input: 'src',
-		output: '_site'
-	},
-	templateFormats: [
-		"md",
-		"html",
-		"css",
-		"jpg",
-		"png",
-		"gif",
-		"ico",
-		"svg",
-		"njk",
-		"ttf",
-		"otf",
-		"woff",
-		"woff2",
-	],
-	passthroughFileCopy: true,
+const markdownIt = require('markdown-it');
+const markdownItAnchor = require('markdown-it-anchor');
+
+module.exports = function(eleventyConfig) {
+
+	const markdownOptions = markdownIt({
+		html: true, 
+		typographer: true
+	}).use(markdownItAnchor, {
+		level: 2, 
+		permalink: true,
+		permalinkClass: 'permalink',
+		permalinkSymbol: '<span role="img" aria-hidden="true" aria-label="permalink">🖇️</span>',
+
+	});
+
+	eleventyConfig.setLibrary('md', markdownOptions);
+
+	return {
+		dir: {
+			input: 'src',
+			output: '_site'
+		},
+		templateFormats: [
+			"md",
+			"html",
+			"css",
+			"jpg",
+			"png",
+			"gif",
+			"ico",
+			"svg",
+			"njk",
+			"ttf",
+			"otf",
+			"woff",
+			"woff2",
+		],
+		passthroughFileCopy: true,
+	}
 }
